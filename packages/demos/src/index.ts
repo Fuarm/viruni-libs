ce529007dd77810052254677f9e97862bb89d401
苟富军 <750108747@qq.com>
2022-05-31 14:16:41 +0800

    fix package code

--- packages/demos/src/index.ts
+++ packages/demos/src/index.ts
@@ -1,23 +1,46 @@
 import data from "@viruni/core"
+import { assign, createMachine, interpret } from "xstate"
 
 type DemoData = {
   userId: number,
   user: string,
-  error: null | Error
+  error: null | Error,
+  data: string
 }
 
-const normalTash = () => {
-  return new Promise<DemoData>((resolve, reject) => {
-    setTimeout(() => {
-      if(Math.random() > 0.5) {
-        resolve({userId: Math.random(), user: "viruni", error: null})
-      } else {
-        reject({userId: 0, user: "", error: new Error('测试')})
-      }
-    }, 3000)
+const normalTash = new Promise<DemoData>((resolve, reject) => {
+  setTimeout(() => {
+    if(Math.random() > 0.5) {
+      resolve({userId: Math.random(), user: "viruni", error: null, data})
+    } else {
+      reject({userId: 0, user: "", error: new Error('测试')})
+    }
+  }, 3000)
+})
+
+const fetchCuteAnimals = () => normalTash
+
+const toggleMachine = createMachine({
+  id: 'toggle',
+  initial: 'inactive',
+  states: {
+    inactive: {
+      on: { TOGGLE: 'active' }
+    },
+    active: {
+      on: { TOGGLE: 'inactive'}
+    }
+  }
+})
+
+const toggleService = interpret(toggleMachine).start()
+toggleService.onTransition(state => {
+  console.log("🚗🚗", state.value)
+  const selectionFired = new CustomEvent('ctoggle', {
+    detail: state.value
   })
-}
+  window.dispatchEvent(selectionFired)
+  window.dispatchEvent(new Event('toggle'))
+})
 
-export { normalTash }
-
-console.log(data)
+export { toggleService, fetchCuteAnimals }
